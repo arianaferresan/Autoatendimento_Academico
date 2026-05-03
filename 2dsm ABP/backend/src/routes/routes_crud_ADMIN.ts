c0def42f29312fe79320715aad6b54e044af1edd
William Max <chwilha2@gmail.com>
2026-05-02 21:07:48 -0300

    Primeira versão do backend

---
+++ 2dsm ABP/backend/src/routes/routes_crud_ADMIN.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { pool } from '@/server/config/database.js';
+import { upload } from '@/server/config/multer.js';
+import { getAllNodes, filterNodes, deleteNode, updateNode, creatNode } from '@/controllers/adminController.js';
+
+const routerADMIN = Router();
+
+// GET all para o admin
+routerADMIN.get('/nodes/all', getAllNodes);
+    
+//GET filter por ID das classes principais (1, 2 ,3 e 4)
+routerADMIN.get('/nodes/filter/:id', filterNodes);
+
+//DELETE por ID para o admin
+routerADMIN.delete('/nodes/:id', deleteNode);
+
+//UPDATE por ID para o admin
+routerADMIN.put('/nodes/:id', upload.single('file'), updateNode);
+
+
+//CREATE para o admin AJUSTAR O UPLOAD VIA MULTER
+routerADMIN.post('/nodes/create', upload.single('file'), creatNode);
+
+
+export default routerADMIN;
---
+++ 2dsm ABP/backend/src/server/config/database.ts
@@ -0,0 +1,21 @@
+import { Pool } from "../../../node_modules/@types/pg/index.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Configuração do Banco usando process.env
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+});
+
+pool.on("connect", () => {
+  console.log("Base de dados conectada com sucesso!");
+});
+
+pool.on("error", (err) => {
+  console.error("Erro na conexão com a base de dados:", err);
+});
